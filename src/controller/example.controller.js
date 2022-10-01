dc31f27f7956efbe3405111ef19a4eb583a175d6
Thanh Tong <thanh.tong@blueink.com>
2022-10-01 12:47:09 -0700

    Improve examples

--- src/controller/example.controller.js
+++ src/controller/example.controller.js
@@ -12,6 +12,7 @@ examples.fileUpload = async (req, res) => {
     if (req.method === "POST") {
         try {
             const file = req.file;
+            const { name, email } = req.body;
 
             const bh = new BundleHelper({
                 label: "New Bundle Label not convert from buffer",
@@ -24,8 +25,8 @@ examples.fileUpload = async (req, res) => {
             const docKey = bh.addDocumentByFile(file);
 
             const signer = bh.addSigner({
-                name: "The Signer One",
-                email: "peter.gibbons@example.com",
+                name,
+                email,
                 deliver_via: "embed",
             });
 
@@ -34,11 +35,14 @@ examples.fileUpload = async (req, res) => {
                 page: 1,
                 kind: "txt",
                 editors: [signer],
+                // initial_value: `Hello ${name}`, - Or we can use initializeValue method
                 x: 15,
                 y: 60,
                 w: 20,
                 h: 3,
             });
+
+            bh.initializeField(docKey, field, `Hello ${name}`);
 
             const newBundle = await client.bundles.create(bh.asData());
             const packetId = newBundle.data.packets[0].id;
@@ -61,6 +65,8 @@ examples.template = async (req, res) => {
     if (req.method === "POST") {
         const TEMPLATE_ID = process.env.TEMPLATE_ID;
 
+        const { name, email, company, title, superhero } = req.body;
+
         try {
             const bh = new BundleHelper({
                 label: "New Bundle Label not convert from buffer",
@@ -75,12 +81,26 @@ examples.template = async (req, res) => {
             });
 
             const signer = bh.addSigner({
-                name: "The Signer One",
-                email: "peter.gibbons@example.com",
+                name,
+                email,
                 deliver_via: "embed",
             });
 
             bh.assignRole(signer, templateKey, "signer-1");
+
+            bh.initializeField(templateKey, "inp001-SzYYb", name);
+
+            if (company) {
+                bh.initializeField(templateKey, "inp002-_4AG5", company);
+            }
+
+            if (title) {
+                bh.initializeField(templateKey, "inp003-eMYsa", title);
+            }
+
+            if (superhero) {
+                bh.initializeField(templateKey, "inp004-q2G4Y", superhero);
+            }
 
             const newBundle = await client.bundles.create(bh.asData());
             const packetId = newBundle.data.packets[0].id;
@@ -151,7 +171,8 @@ examples.url = async (req, res) => {
 examples.b64 = async (req, res) => {
     if (req.method === "POST") {
         try {
-            const { file_b64 } = req.body;
+            const { file_b64, name, email, company, title, superhero } =
+                req.body;
 
             const bh = new BundleHelper({
                 label: "New Bundle Label not convert from buffer",
@@ -164,21 +185,78 @@ examples.b64 = async (req, res) => {
             const docKey = bh.addDocumentByB64(file_b64);
 
             const signer = bh.addSigner({
-                name: "The Signer One",
-                email: "peter.gibbons@example.com",
+                name,
+                email,
                 deliver_via: "embed",
             });
 
-            const field = bh.addField(docKey, {
+            const nameField = bh.addField(docKey, {
                 label: "Your Name",
                 page: 1,
                 kind: "txt",
                 editors: [signer],
-                x: 15,
-                y: 60,
+                x: 43.47,
+                y: 76.04,
                 w: 20,
+                h: 1.5,
+            });
+
+            const companyField = bh.addField(docKey, {
+                label: "Your Company",
+                page: 1,
+                kind: "txt",
+                editors: [signer],
+                x: 43.47,
+                y: 72.39,
+                w: 20,
+                h: 1.5,
+            });
+
+            const titleField = bh.addField(docKey, {
+                label: "Your Title",
+                page: 1,
+                kind: "txt",
+                editors: [signer],
+                x: 43.47,
+                y: 68.87,
+                w: 20,
+                h: 1.5,
+            });
+
+            const heroField = bh.addField(docKey, {
+                label: "Your favorite superhero",
+                page: 1,
+                kind: "txt",
+                editors: [signer],
+                x: 43.47,
+                y: 65.23,
+                w: 20,
+                h: 1.5,
+            });
+
+            const initialField = bh.addField(docKey, {
+                page: 1,
+                kind: "ini",
+                editors: [signer],
+                x: 16.12,
+                y: 53.92,
+                w: 5,
                 h: 3,
             });
+
+            bh.initializeField(docKey, nameField, name);
+
+            if (company) {
+                bh.initializeField(docKey, companyField, company);
+            }
+
+            if (title) {
+                bh.initializeField(docKey, titleField, title);
+            }
+
+            if (superhero) {
+                bh.initializeField(docKey, heroField, superhero);
+            }
 
             const newBundle = await client.bundles.create(bh.asData());
             const packetId = newBundle.data.packets[0].id;
